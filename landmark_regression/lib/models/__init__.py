9784ce1c4e04bd38b204200f36309ac60120b586
BoChenYS <g55kern@gmail.com>
2019-09-05 17:37:18 +0930

    change landmark_regression/lib/models/__init__.py

--- landmark_regression/lib/models/__init__.py
+++ landmark_regression/lib/models/__init__.py
@@ -12,7 +12,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import models.pose_resnet
-import models.pose_hrnet
-import models.my_pose_hrnet
-import models.my2
+import models.my_hrnet768
+
